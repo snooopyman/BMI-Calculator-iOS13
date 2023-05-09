aff47ccff23c6c3afa13cf09ee7588f725ca085e
Snooopyman <armando.snoop@gmail.com>
2023-05-09 14:27:00 +0200

    creating label, sliders and Class

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Armando Cáceres on 9/5/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
