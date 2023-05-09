4df689fc3ec3261b444a8681443f2e6b1f8106b3
Snooopyman <armando.snoop@gmail.com>
2023-05-09 14:48:10 +0200

    SeconViewController -> how to create a ViewController from code

--- BMI Calculator/Controllers/SecondViewController.swift
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -1,16 +1,26 @@
 //
 //  SecondViewController.swift
 //  BMI Calculator
-//
-//  Created by Armando Cáceres on 9/5/23.
-//  Copyright © 2023 Angela Yu. All rights reserved.
-//
+//__________________________________________
+// HOW TO CREATE A VIEWCONTROLLER FROM CODE
+//------------------------------------------
 
+/*
 import UIKit
 
-class secondViewController: UIViewController {
+class SecondViewController: UIViewController {
+    
+    var bmiValue = "0.0"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        
+        let label = UILabel()
+        label.text = bmiValue
+        label.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        view.addSubview(label)
     }
 }
+*/
